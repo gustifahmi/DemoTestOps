73e0f8522b7dadbf97be8e0c494775c57b1ce92a
Gusti Fahmi Fadhila <gustifahmifadhila@gmail.com>
2022-07-04 08:39:38 +0700

    add sesi 6

---
+++ Test Suites/Sesi 6/Demo Schedule.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo Schedule</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8eef130a-f3d0-461a-8137-781fa2f64583</testSuiteGuid>
+   <testCaseLink>
+      <guid>747c80c2-c0ea-47dc-ab94-b2eb1eabfb2b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sesi 6/Demo Login Cura</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>68e12444-5430-42d0-bc1b-578269f41fac</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e420704d-5238-4c98-b40a-8106304a1622</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
